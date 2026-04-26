61573c9afc559e90f78edb2b6bc67300509f2a56
Blitical <110457539+Blitical@users.noreply.github.com>
2026-04-26 15:39:54 +0100

    build: Add a shadowJar builder for "core" and "processor" modules & decentralise versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,38 @@
+plugins {
+    java
+    id("com.gradleup.shadow") version "8.3.3"
+}
+
+group = "dev.blitical"
+version = project.version
+
 allprojects {
     repositories {
         mavenCentral()
     }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":processor"))
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.shadowJar {
+    archiveBaseName.set(rootProject.name)
+    archiveVersion.set(project.version.toString())
+    archiveClassifier.set("") // removes "-all"
+
+    from(project(":core").sourceSets.main.get().output)
+    from(project(":processor").sourceSets.main.get().output)
+    manifest {
+        attributes["Automatic-Module-Name"] = "dev.blitical.jigsawDB"
+    }
+
+    configurations = listOf(project.configurations.runtimeClasspath.get())
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    mergeServiceFiles()
 }
