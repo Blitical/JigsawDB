8f587ce3990005bcde2ab4b6506bd536523db5c2
Blitical <110457539+Blitical@users.noreply.github.com>
2026-05-25 16:21:09 +0100

    fix: Fix ExecutableMetadataGetter

--- src/core/src/main/java/dev/blitical/jigsawDB/value/ExecutableMetadataGetter.java
+++ src/core/src/main/java/dev/blitical/jigsawDB/value/ExecutableMetadataGetter.java
@@ -1,6 +1,10 @@
 package dev.blitical.jigsawDB.value;
 
+import java.util.concurrent.CountDownLatch;
+
 public class ExecutableMetadataGetter {
+    private final CountDownLatch latch = new CountDownLatch(1);
+    private Result result = null;
 
     public record Result(
             Long start,
@@ -11,5 +15,29 @@ public class ExecutableMetadataGetter {
     }
 
     protected void setResult(Result result) {
+        this.result = result;
+        latch.countDown();
+    }
+
+    public Result awaitResult() throws InterruptedException {
+        latch.await();
+        return result;
+    }
+
+    public Result awaitResultWithoutException() {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public boolean resultExists() {
+        return result != null;
     }
 }
